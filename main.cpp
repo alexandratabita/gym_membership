faabb94effdae3fc56dafa553c7f7e1df1a8e02d
niel1603 <alexanderdaniel.1603@outlook.com>
2024-11-25 17:25:44 +0700

    feat: loop through program

--- main.cpp
+++ main.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class User {
     public:
     void ViewJadwal() {
+        system("cls");
         cout << "Jadwal Buka" << endl;
-        cout << "==============" << endl;
+        cout << "----------------------" << endl;
         cout << "Senin" << "  | " << "04.30 - 22.00" << endl;
         cout << "Selasa" << " | " << "04.30 - 22.00" << endl;
-        cout << "Rabu" << "   | " << "04.30 -  22.00" << endl;
+        cout << "Rabu" << "   | " << "04.30 - 22.00" << endl;
         cout << "Kamis" << "  | " << "04.30 - 22.00" << endl;
-        cout << "Jumat" << "  | " << "04.30 -  22.00" << endl;
+        cout << "Jumat" << "  | " << "04.30 - 22.00" << endl;
         cout << "Sabtu" << "  | " << "04.30 - 22.00" << endl;
+        cout << "Minggu" << " | " << "04.30 - 22.00" << endl;
+        cout << "\n----------------------" << endl;
+        char resp;
+        cout << "Kembali ke menu awal? (Y): ";
+        cin >> resp;
     }
     void ViewPelatih() {
+        system("cls");
         cout << "Jadwal Pelatih" << endl;
-        cout << "==============" << endl;
-        cout << "Pelatih 1: Ronaldo" << "      | " << "Senin, Rabu" << endl;
-        cout << "Pelatih 2: Messi" << "        | " << "Rabu, Kamis" <<endl;
-        cout << "Pelatih 3: Ronaldinho" << "   | " << "Selasa, Jumat" << endl;
-        cout << "Pelatih 4: Xavi" << "         | " << "Rabu, Sabtu" << endl;
-        cout << "Pelatih 5: Cristiano" << "    | " << "Kamis, Sabtu" << endl;
-        cout << "Pelatih 6: Lionel Messi" << " | " << "Senin, Selasa" << endl;
+        cout << "----------------------------------------" << endl;
+        cout << "Pelatih 1: Rizky Pratama" << " | " << "Senin, Jumat" << endl;
+        cout << "Pelatih 2: Raditya Putra" << " | " << "Rabu, Kamis" <<endl;
+        cout << "Pelatih 3: Daniel Aditya" << " | " << "Selasa, Jumat" << endl;
+        cout << "Pelatih 4: Kevin Permana" << " | " << "Rabu, Sabtu" << endl;
+        cout << "Pelatih 5: Satria Wijaya" << " | " << "Kamis, Sabtu" << endl;
+        cout << "Pelatih 6: Andreas Dimas" << " | " << "Senin, Selasa" << endl;
+        cout << "\n----------------------------------------" << endl;
+        char resp;
+        cout << "Kembali ke menu awal? (Y): ";
+        cin >> resp;
     }
 };
 
@@ -34,33 +46,42 @@ const int KELUAR = 99;
 int getUserInput();
 int main() {
     User user;
-    int choice = getUserInput();
-    switch (choice) {
-        case KELUAR:
-            cout << "Terima kasih sudah menggunakan layanan kami." << endl;
+    int shouldExit = 0;
+    while (true) {
+        if (shouldExit) {
             break;
-        case JADWAL:
-            user.ViewJadwal();
-            break;
-        case PELATIH:
-            user.ViewPelatih();
-            break;
-         default:
-            cout << "Pilihan yang anda masukkan salah." << endl;
-            break;
+        }
+        int choice = getUserInput();
+        switch (choice) {
+            case KELUAR:     
+                cout << "Terima kasih sudah menggunakan layanan kami." << endl;
+                shouldExit = 1;
+                break;
+            case JADWAL:
+                user.ViewJadwal();
+                break;
+            case PELATIH:
+                user.ViewPelatih();
+                break;
+            default:
+                cout << "Pilihan yang anda masukkan salah." << endl;
+                break;
+        }
     }
     return 0;
 }
 
 int getUserInput() {
     int choice;
+    system("cls");
     cout << "Pilih menu:" << endl;
     cout << "1. Lihat jadwal buka" << endl;
     cout << "2. Lihat jadwal pelatih" << endl;
     cout << "3. Buat janji" << endl;
     cout << "4. Login admin" << endl;
     cout << "99. Keluar" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Masukkan pilihan: ";
     cin >> choice;
-    return choice;
-
+    return choice; 
 }
