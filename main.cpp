774955e86e07be43a697c3943827d8bfd3c1207f
niel1603 <alexanderdaniel.1603@outlook.com>
2024-11-27 17:39:10 +0700

    feat : changes

--- main.cpp
+++ main.cpp
@@ -16,9 +16,9 @@ class User {
         cout << "Sabtu" << "  | " << "04.30 - 22.00" << endl;
         cout << "Minggu" << " | " << "04.30 - 22.00" << endl;
         cout << "\n----------------------" << endl;
-        char resp;
-        cout << "Kembali ke menu awal? (Y): ";
-        cin >> resp;
+        cout << "Tekan tombol Enter untuk kembali...";
+        cin.ignore();
+        cin.get();
     }
     void ViewPelatih() {
         system("cls");
@@ -31,9 +31,67 @@ class User {
         cout << "Pelatih 5: Satria Wijaya" << " | " << "Kamis, Sabtu" << endl;
         cout << "Pelatih 6: Andreas Dimas" << " | " << "Senin, Selasa" << endl;
         cout << "\n----------------------------------------" << endl;
-        char resp;
-        cout << "Kembali ke menu awal? (Y): ";
-        cin >> resp;
+        cout << "Tekan tombol Enter untuk kembali...";
+        cin.ignore();
+        cin.get();
+    }
+};
+    
+class Admin : public User {
+    private :
+    string name;
+    string password;
+
+        bool _validateLogin(string _name, string _password, Admin arrAdmin[10]) {
+            bool isSuccess = false;
+            for (int i = 0; i < 10; i++) {
+                if (_name == arrAdmin[i].name && _password == arrAdmin[i].password) {
+                isSuccess = true;
+                break;
+                }
+            }
+            if (isSuccess) {
+                cout << "Login success." << endl;
+                return isSuccess;
+
+                
+            }
+            cout << "Invalid username or password. Please try again." << endl;
+            return isSuccess;
+        }
+
+    public :
+    Admin(string _name, string _password) {
+        Admin::name = _name;
+        Admin::password = _password;
+    }
+
+    Admin(){}
+    void LoginAdmin(Admin arrAdmin[10]) {
+        string _name, _password;
+        system("cls");
+        cout << "LOGIN" << endl;
+        cout << "-----------" << endl;
+        cout << "username: ";
+        cin >> _name;
+        cout << "password: ";
+        cin >> _password;
+
+        cout << "-----------" << endl;
+        Admin::_validateLogin(_name, _password, arrAdmin);
+
+        cout << "press enter to continue...";
+        cin.ignore();
+        cin.get();
+    }
+    void CreateMembership() {
+        
+    }
+    void EditMembership() {
+    }
+    void DeleteMembership() {
+    }
+    void ViewMembership() {
     }
 };
 
@@ -46,6 +104,12 @@ const int KELUAR = 99;
 int getUserInput();
 int main() {
     User user;
+    Admin admin;
+
+    Admin arrAdmin[10];
+    arrAdmin[0] = Admin("bitalxndr", "pw1234");
+    arrAdmin[1] = Admin("bittabitt", "pw1235");
+
     int shouldExit = 0;
     while (true) {
         if (shouldExit) {
@@ -62,6 +126,9 @@ int main() {
                 break;
             case PELATIH:
                 user.ViewPelatih();
+                break;
+            case LOGIN:
+                admin.LoginAdmin(arrAdmin);
                 break;
             default:
                 cout << "Pilihan yang anda masukkan salah." << endl;
@@ -84,4 +151,19 @@ int getUserInput() {
     cout << "Masukkan pilihan: ";
     cin >> choice;
     return choice; 
+}
+int UIAdmin() {
+    Admin admin;
+    int choice;
+    system("cls");
+    cout << "Pilih menu:" << endl;
+    cout << "1. Create Membership" << endl;
+    cout << "2. Edit Membership" << endl;
+    cout << "3. Delete Membership" << endl;
+    cout << "4. View Membership" << endl;
+    cout << "5. Keluar" << endl;
+    cout << "--------------------" << endl;
+    cout << "Masukkan Pilihan: ";
+    cin >> choice;
+    return choice;
 }
